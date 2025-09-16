954f826191405a781b4d9172e7646292a8e96c7b
giovannicg <60613860+giovannicg@users.noreply.github.com>
2025-09-16 22:55:27 +0200

    Backend in terraform

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -38,13 +38,7 @@ variable "eks_node_count" {
 variable "eks_instance_type" {
   type        = string
   description = "EKS node instance type"
-  default     = "t2.micro"
-}
-
-variable "mongodb_instance_class" {
-  type        = string
-  description = "DocumentDB instance class"
-  default     = "db.t3.medium"
+  default     = "t3.medium"
 }
 
 
